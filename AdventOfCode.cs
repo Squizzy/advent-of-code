7a7ff32d07ab5464bcfaa2e631045525cd5e5108
Squizzy <crazy_squirrel@yahoo.com>
2025-12-02 15:54:43 +0000

    Some attempts at simplifying the error checker

--- AdventOfCode.cs
+++ AdventOfCode.cs
@@ -1,27 +1,52 @@
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using AoC2025;
+using Microsoft.VisualBasic;
 
 namespace AdventOfcode
 {
     class AdventOfCode
     {
+
+        // private static (T, bool) get_new_day<T>(T day)
+        // {
+        //     try {
+        //         day = new(); 
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Console.WriteLine($"Error: {e}");
+        //         return (day, null);
+        //     }
+        //     return (day, null);
+        // }
+
+        // public int run_day<T>(T day)
+        // {
+        //     if (!day.DayDataLoadedSuccessfully) return 1;
+        //     // day.DisplayData();
+        //     day.Part1();
+        //     day.Part2();
+
+        //     return 0;
+        // }
+
         public static int Main()
         {
             Console.WriteLine("Advent Of Code");
 
-            Day1 day;
-            try {
-                day = new(); 
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error: {e}");
-                return 1;
-            }
-
+            Day1 day1 = new();
+            if (!day1.DayDataLoadedSuccessfully) return 1;
             // day.DisplayData();
-            day.Part1();
-            day.Part2();
+            day1.Part1();
+            day1.Part2();
+                
+            Day2 day2 = new();
+            if (!day2.DayDataLoadedSuccessfully) return 1;
+            // day.DisplayData();
+            day2.Part1();
+            day2.Part2();
+
 
             // Day2 day2 = new();
             // // day2.DisplayDay2Data();
